f88cdc222e51294000be3a955a5f98a88a560021
ArmeinDul <a.m.dul@student.utwente.nl>
2022-03-23 14:47:16 +0100

    Added guess words

--- gameTopicsCollection.py
+++ gameTopicsCollection.py
@@ -1,40 +1,187 @@
 from GuessWord import GuessWord
 
 # TODO all these cases are test cases. They need to be redone with actual content.
-RANDOM_NAME_1 = GuessWord('henk',
+TEL_PROGRAM_1 = GuessWord('peppa pig',
                           [
-                              "Hij is kaal",
-                              "Zijn achternaam is maathuis",
-                              "Hij is een steen",
-                              "Hij heeft een echte opa naam",
-                              "Zijn step is geel met blauwe wielen (vet lelijk)",
+                              "The animal has the colour pink #myfavouritecolour 💖",
+                              "The name of the brother is George #bestfriends 👫👭👬",
+                              "One of the best friend’s is called Zoë Zebra #bestfriends 👫👭👬",
+                              "Her family and she like to snort, like “oink oink” #funny 🐷",
+                              "She wears a red dress #beautiful🤩 ",
                           ]
                           )
-RANDOM_NAME_2 = GuessWord('barry',
+TEL_PROGRAM_2 = GuessWord('sesame street',
                           [
-                              "Hij is super hip",
-                              "Hij houdt van disco dip op zijn ijsjes",
-                              "Hij heeft nog nooit op een duikplank gestaan",
-                              "Hij kent 7 verschillende katten feitjes",
-                              "Zijn step is paars met rode wielen",
+                              "A big bird is often on screen #dontflyaway 🕊️",
+                              "One character loves their cookies #yummy 🍪🍪🍪",
+                              "A good bed time story #letsread 📚",
+                              "It is a street with lovely puppets #elmo 🥺",
+                              "It is a tv program on tv at 12 and 6 o’clock #neverenough ⏰",
                           ]
                           )
 
-RANDOM_COLOR_1 = GuessWord('paars',
+TEL_PROGRAM_3 = GuessWord('dora',
                            [
-                               "Het is niet geel",
-                               "Het is niet oranje",
-                               "Het lijkt op laars",
-                               "Zijn step is rood met rode wielen"
+                               "This television program is about a girl, who is going on an adventure 🎒",
+                               "She explores the whole world with a map #onthemap #onthemap 🗺️",
+                               "Her best friend is a monkey called Boots #lovesbananas 🥾",
+                               "Another best friend is called Diego #adventurer 🌳",
+                               "Her enemy is a mean fox, who wants to steal her map #notverynice 🦊",
                            ]
                            )
 
-GAME_TOPIC_NAMES = [RANDOM_NAME_1, RANDOM_NAME_2]
-GAME_TOPIC_COLORS = [RANDOM_COLOR_1]
+NATURE_1 = GuessWord('Rain',
+                          [
+                              "It is water #transparent🌊🌊",
+                              "You don’t want to cycle when this happens #gettingwet 🌧️⛈",
+                              "It is coming down from the sky #woosh🌈⛱️",
+                              "They are droplets #fallingtopieces 💧💦",
+                              "You would want to wear a different coat for this kind of weather #beautiful🧥❤️",
+                          ]
+                          )
+NATURE_2 = GuessWord('Thunder',
+                          [
+                              "It is a flash of light #kapow ⚡🌩️🚨 ",
+                              "It is dangerous to cycle when this happens #lightning 🌦️🌂 ",
+                              "They are reaching for the highest point #trees #tallbuildings  ",
+                              "It could electrocute you #electricity 🔦💡 ",
+                              "Be aware of water, when you are standing outside #avoid 🏕️🥾 ",
+                          ]
+                          )
+NATURE_3 = GuessWord('Earthquake',
+                          [
+                              "Shaking due to waves #notwater 🤝🏄",
+                              "The world is shaking #leftright🌍 ",
+                              "Holes could be created in the soil #hole-in-one ⛳🕳️ ",
+                              "The intensity of this phenomena is measured on the Richter scale #calculation 🧮😌",
+                              "Two parts of the world are crashing #boom 💥💣",
+                          ]
+                          )
 
-KID_TOPICS_PLAYGROUND = ["names", "colors"]
-KID_TOPICS_HOME = ["television", "sheep"]
-ADOLESCENT_TOPICS_NETFLIX = ["alcohol", "drugs"]
-ADOLESCENT_TOPICS_MUSIC = ["drugs", "drugs"]
-ADULT_TOPICS_PIZZA = ["you", "do", "not", "want", "to", "know"]
-ADULT_TOPICS_DINNER = ["you", "do", "", "want", "to", "know"]
+DUTCH_MUSIC_1 = GuessWord('gebroeders scooter',
+                          [
+                              "Mix of music #k3lovesTess 🎵",
+                              "This is a long pre-defined playlist #dancingtilllwedrop 💃🕺",
+                              "You should drink at the honking #toooot 📣📣📣",
+                              "Guess the song! #karaoketonight🎤 ",
+                              "Totale escalatie #wildingg 🔥",
+                          ]
+                          )
+DUTCH_MUSIC_2 = GuessWord('toppers',
+                          [
+                              "They like to dance and sing #swingingforlife 💃🕺",
+                              "They love their glitter costumes #glittermeup 🌟",
+                              "These 4 are Dutch icons #bestofthenetherlands 🇳🇱",
+                              "Two of the members present Geer en Goor #legends 🙌",
+                              "One of the members is called Gerard Joling #Geeeer 😎",
+                          ]
+                          )
+DUTCH_MUSIC_3 = GuessWord('dries roelvink',
+                          [
+                              "He likes to wear yellow swimshorts #summertime 🌞",
+                              "He lives in Amsterdam #theplace2be 🌇",
+                              "He has three sons #onehappybigfamily 👨‍👩‍👦‍👦",
+                              "Together with his family, he has a realty program #famous ⭐",
+                              "His most famous song is “Ik kom eraan!” #run🏃️🏃 ",
+                          ]
+                          )
+
+GEOGRAPHY_1= GuessWord('italy',
+                          [
+                             "The flag is green, white and red #flaggy🚩🚩🏁",
+                              "Ruled by Mario Draghi #luigi🍄🍀",
+                              "Home country of the delicious dish pizza #can’tgetenoughofit🍕🍕🍕🍕",
+                              "This is the country with a leaning tower #Pisa #cake 🗼🍰",
+                              "No fighting in the building anymore #spartacus 🏰🏯",
+                          ]
+                          )
+GEOGRAPHY_2 = GuessWord('netherlands',
+                        [
+                            "They looooooooove cheese #Kaaskop🧀😀",
+                            "It’s a small country #lessisalwaysbetter🤏🥰",
+                            "Tulips are a great flower #tulipfields🌷🌷",
+                            "Clogs are great #stereotypical👞👡🩴👞 ",
+                            "Tall people also matter #help 👥🆘",
+                        ]
+                        )
+GEOGRAPHY_3 = GuessWord('United Kingdom',
+                        [
+                            "Standing in a queue is a great hobby #waitingnohating 👬👬👬👬👬👬",
+                            "Overly polite people #pleasethankyou🥺🙏 ",
+                            "Tea without milk is no tea #ellogovna🫖☕ ",
+                            "The queen is well protected #warmhats 👑👑👸🏼",
+                            "Telephone boxes surround you #redisnoyellow📞❤️",
+                        ]
+                        )
+
+FILMS_1 = GuessWord('James Bond',
+                          [
+                              "The main character was created by Ian Fleming #greatman 🕴️🧳",
+                              "He liked his martinis #shakennotstirred🍸",
+                              "Sean Connery and Roger Moore both appeared in 7 of these films #madetheircareers🥳 ",
+                              "Daniel Craig has just stopped playing this character #sad 😥",
+                              "For the new movies, there has been a big change: A woman is taking over #notdoctorwho 💃",
+                          ]
+                          )
+FILMS_2 = GuessWord('mr bean',
+                          [
+                              "The man with the vegetable name #notcauliflower",
+                              "He does not like talking #introvert💬 ",
+                              "He is very clumsy #fallingoverisokay🥸 ",
+                              "He has a brown puppet #cute 🤩",
+                              "He aaaaalways wears a suit #suitup🕴️🧳",
+                          ]
+                          )
+
+FILMS_3 = GuessWord('home alone',
+                          [
+                              "A classic Christmas film #Kevin 🎄",
+                              "The film was directed by Chris Columbus #youreawizardharry 🧙‍",
+                              "The main character likes building obstacle courses #beware 🪵",
+                              "Parents go on holiday #newyork🌆 ",
+                              "Little kid left alone #watchoutforthethiefs 🧒👦",
+                          ]
+                          )
+MUSIC_1 = GuessWord('Michael Jackson',
+                    [
+                        "Also called the “King of pop” #iconsneverdie👑🍾" ,
+                        "Be careful of the zombies #thrilledaboutlife 🧟‍🧟️",
+                        "His moves take you to the moon and back #reversewalking 💃🕺",
+                        "He likes dancing in the mirror #heisaman 🪞",
+                        "Started out as part of the Jackson Brothers #bros 😎👊",
+                    ]
+                    )
+MUSIC_2 = GuessWord('Whitney Houston',
+                    [
+                        "She was the best-selling female R&B artist of the 20th century #winning🤵️👨‍🎨",
+                        "She just wants to dance with someone #feeltheheat👯️👯‍",
+                        "Last name is a city in Texas #nasawehaveaproblem 🌆",
+                        "She will always love you #cute ❤️❤️❤️",
+                        "Dolly Parton has covered one of her most famous songs “I will always love you” #friendshipgoals",
+                    ]
+                    )
+
+MUSIC_3 = GuessWord('Queen',
+                    [
+                        "He loves showing off #chesthair💁‍💇‍",
+                        "A tiara is not fancy enough for her #crown 👸🏼👸🏼☠️",
+                        "It is the opposite a King #royals 👑👑",
+                        "We will, we will rock you! #applause 👏",
+                        "The last name of the singer is named after a planet #notvenus🎤🎹 ",
+                    ]
+                    )
+
+GAME_TOPIC_TEL_PROGRAMS = [TEL_PROGRAM_1, TEL_PROGRAM_2, TEL_PROGRAM_3]
+GAME_TOPIC_NATURE = [NATURE_1, NATURE_2, NATURE_3]
+GAME_TOPIC_DUTCH_MUSIC = [DUTCH_MUSIC_1, DUTCH_MUSIC_2, DUTCH_MUSIC_3]
+GAME_TOPIC_GEOGRAPHY = [GEOGRAPHY_1,GEOGRAPHY_2,GEOGRAPHY_3]
+GAME_TOPIC_FILMS = [FILMS_1,FILMS_2,FILMS_3]
+GAME_TOPIC_MUSIC = [MUSIC_1,MUSIC_2,MUSIC_3]
+
+
+KID_TOPICS_PLAYGROUND = ["nature", ""]
+KID_TOPICS_HOME = ["television", ""]
+ADOLESCENT_TOPICS_NETFLIX = ["geography", ""]
+ADOLESCENT_TOPICS_MUSIC = ["dutch music", ""]
+ADULT_TOPICS_PIZZA = ["films", ""]
+ADULT_TOPICS_DINNER = ["music", ""]
